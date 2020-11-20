a9443a6fba4aa055c5acaed0f4c39aa35670d9b5
Seunghwa3 <tmdghk3636@naver.com>
2020-11-20 19:49:14 +0900

    new

--- polls/views.py
+++ polls/views.py
@@ -12,30 +12,6 @@ from rest_framework.response import Response
 from rest_framework import status
 
 # Create your views here.
-"""
-@api_view(['GET', 'POST'])
-def item_list(request):
-	print(request)
-	if request.method == 'GET':
-		print('get')
-
-		area = self.request.query_params.get('area')
-    	age= self.request.query_params.get('age')
-    	gender = self.request.query_params.get('gender')
-		print(gender)
-		quaryset = Receive.objects.all()
-		serializer = ReceiveSerializer(quaryset, many=True)
-		return JsonResponse(serializer.data, safe=False)
-	
-	elif request.method == 'POST':
-		print('post')
-		data = JSONParser().parse(request)
-		serializer = SendSerializer(data=data)
-		if serializer.is_valid():
-			serializer.save()
-			return JsonResponse(serializer.data, status=201)
-		return JsonResponse(serializer.errors, status=400)
-"""
 
 class SnippetList(APIView):
 
