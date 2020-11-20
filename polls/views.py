29caad7202c1ae0a0f2949fde15227dd5904a092
Seunghwa3 <tmdghk3636@naver.com>
2020-11-20 19:11:28 +0900

    new

--- polls/views.py
+++ polls/views.py
@@ -12,11 +12,6 @@ def item_list(request):
 	print(request)
 	if request.method == 'GET':
 		print('get')
-
-		area = self.request.queryset.get('area')
-    	age= self.request.queryset.get('age')
-    	gender = self.request.queryset.get('gender')
-		
 		quaryset = Receive.objects.all()
 		serializer = ReceiveSerializer(quaryset, many=True)
 		return JsonResponse(serializer.data, safe=False)
