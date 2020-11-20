eb64ab7d0209405806625aa50459f3f89c8d63db
Seunghwa3 <tmdghk3636@naver.com>
2020-11-20 19:47:28 +0900

    new

--- polls/views.py
+++ polls/views.py
@@ -6,7 +6,13 @@ from rest_framework.decorators import api_view
 from polls.models import Send, Receive
 from polls.serializer import ReceiveSerializer, SendSerializer
 
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 # Create your views here.
+"""
 @api_view(['GET', 'POST'])
 def item_list(request):
 	print(request)
@@ -29,4 +35,18 @@ def item_list(request):
 			serializer.save()
 			return JsonResponse(serializer.data, status=201)
 		return JsonResponse(serializer.errors, status=400)
+"""
 
+class SnippetList(APIView):
+
+    def get(self, request, format=None):
+        receive = Receive.objects.all()
+        serializer = ReceiveSerializer(receive, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = SendSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
