09057ef783b41da717f86f9fc448c4063030ce0b
Seunghwa3 <tmdghk3636@naver.com>
2020-11-21 15:47:23 +0900

    -

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,7 @@
 import json
 
-from django.http import JsonResponse
-from rest_framework.parsers import JSONParser
-from rest_framework.decorators import api_view
 from polls.models import Send, Receive
 from polls.serializer import ReceiveSerializer, SendSerializer
-
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -16,15 +12,6 @@ from rest_framework import status
 class SnippetList(APIView):
 
     def get(self, request, format=None):
-        Area = Receive.objects.values("area")
-        print(Area)
         send = Send.objects.all().filter(product_name='A')
         serializer = SendSerializer(send, many=True)
         return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = ReceiveSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
