c44126956f776881d73a63b595fb13327ace5fc1
Seunghwa3 <tmdghk3636@naver.com>
2020-11-21 15:56:50 +0900

    --

--- polls/views.py
+++ polls/views.py
@@ -15,3 +15,10 @@ class SnippetList(APIView):
         send = Send.objects.all().filter(product_name='A')
         serializer = SendSerializer(send, many=True)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = ReceiveSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
