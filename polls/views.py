8c30bef58b9f59a5359e5570e848de9bd31409bf
Seunghwa3 <tmdghk3636@naver.com>
2020-11-22 19:05:27 +0900

    --

--- polls/views.py
+++ polls/views.py
@@ -12,7 +12,7 @@ from rest_framework import status
 class SnippetList(APIView):
         
     def get(self, request, format=None):
-        send = Send.objects.all().filter(product_name=request)
+        send = Send.objects.all()
         serializer = SendSerializer(send, many=True)
         return Response(serializer.data)
 
