4feb073cc76874d2f49e1d45fcc750c6036360a3
Seunghwa3 <tmdghk3636@naver.com>
2020-11-22 18:40:25 +0900

    --

--- polls/views.py
+++ polls/views.py
@@ -10,8 +10,15 @@ from rest_framework import status
 # Create your views here.
 
 class SnippetList(APIView):
-
+        
     def get(self, request, format=None):
-        send = Send.objects.all().filter(product_name='A_product')
+        send = Send.objects.all().filter(product_name=request)
         serializer = SendSerializer(send, many=True)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = ReceiveSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
