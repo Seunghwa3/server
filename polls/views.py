71fbefa4a89d8dbbc821baf6e1c4c71697f21f8c
Seunghwa3 <tmdghk3636@naver.com>
2020-11-25 00:32:44 +0900

    --

--- polls/views.py
+++ polls/views.py
@@ -15,8 +15,10 @@ class PostList(APIView):
     def get(self, request, format=None):
         area = request.GET['area']
         user_id = request.GET['user_id']
-        product_id = svdclass.svd(user_id, area)
-        send = Send.objects.all()
+        tmp = svdclass.svd(user_id,area)
+        for i in tmp:
+            product_id = i
+            send = Send.objects.all().filter(product_id=product_id)
         serializer = SendSerializer(send, many=True)
         return Response(serializer.data)
 
