e78e2c237e13e37b9f232daf2ae45530824a364a
Seunghwa3 <tmdghk3636@naver.com>
2020-11-25 01:42:56 +0900

    --

--- polls/views.py
+++ polls/views.py
@@ -7,18 +7,27 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from polls.svd_recommend2 import svdclass
+from django.db.models import Q
 
 # Create your views here.
 
 class PostList(APIView):
 
     def get(self, request, format=None):
+
+        
         area = request.GET['area']
         user_id = request.GET['user_id']
+        my_filter_qs = Q()
+        for creator in product_id:
+            my_filter_qs = my_filter_qs | Q(creator=creator)
+        send=Send.objects.all().filter(my_filter_qs)
         tmp = svdclass.svd(user_id, area)
+        """
         for i in tmp :
             product_id = i
             send = Send.objects.all().filter(product_id=product_id)
+        """
         serializer = SendSerializer(send, many=True)
         return Response(serializer.data)
 
