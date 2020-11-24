cf16c590baa01ad5eb507a29ee3b53b50a5bc5f1
Seunghwa3 <tmdghk3636@naver.com>
2020-11-25 01:56:53 +0900

    --

--- polls/views.py
+++ polls/views.py
@@ -14,20 +14,23 @@ from django.db.models import Q
 class PostList(APIView):
 
     def get(self, request, format=None):
-        """
+        
         area = request.GET['area']
         user_id = request.GET['user_id']
         my_filter_qs = Q()
         for creator in product_id:
             my_filter_qs = my_filter_qs | Q(creator=creator)
-        send=Send.objects.all().filter(my_filter_qs)
+            send=Send.objects.all().filter(my_filter_qs)
+            serializer = SendSerializer(send, many=True)
+            return Response(serializer.data)
         tmp = svdclass.svd(user_id, area)
         """
         for i in tmp :
             product_id = i
             send = Send.objects.all().filter(product_id=product_id)
-            serializer = SendSerializer(send, many=True)
-            return Response(serializer.data)
+        """
+        serializer = SendSerializer(send, many=True)
+        return Response(serializer.data)
 
     def post(self, request, format=None):
         serializer = SendSerializer(data=request.data)
