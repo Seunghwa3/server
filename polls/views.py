0d393e0cdf27c849bf5c3d02e31feb7a6b0cb819
Seunghwa3 <tmdghk3636@naver.com>
2020-12-08 19:08:01 +0900

    --

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,11 @@
 import json
 
+import os
+import csv
+import pandas as pd
+from surprise import SVDpp
+from surprise import Dataset
+from surprise import Reader
 from surprise import dump
 from surprise.model_selection import train_test_split
 
@@ -17,8 +23,9 @@ class PostList(APIView):
     def get(self, request, format=None):
         area = request.GET['area']
         user_id = request.GET['user_id']
-
-            algo=SVDpp()
+        
+        def svd(userID, nowarea) :
+            algo = SVDpp()
             algo = SVDpp(n_factors=100, n_epochs=15)
             # 3. train model 저장
             file_name=os.path.expanduser('./dump')
@@ -40,6 +47,11 @@ class PostList(APIView):
             top_product_id = [int(pred.iid) for pred in predictions]
             top_product_id=top_product_id[:5]
             return top_product_id
+            #fortest
+        tmp=svdclass.svd("A2CX7LUOHB2NDG", 'A')
+        # return vlaue :
+        #[594450209, 321732944, 594287995, 594033926, 594033934, 594296420, 594033896, 511189877, 594450705, 594451647, 594477670, 594017343, 594017580, 594202442, 594481813, 558835155, 594478162, 594012015, 439886341, 528881469]
+        print(tmp[0])
 
         send = Send.objects.all().filter(product_id=top_product_id)
         serializer = SendSerializer(send, many=True)
