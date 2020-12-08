7c2955b4483aaa01b858d11d778c493bc10aaa22
Seunghwa3 <tmdghk3636@naver.com>
2020-12-08 22:38:48 +0900

    --

--- polls/views.py
+++ polls/views.py
@@ -24,36 +24,36 @@ class PostList(APIView):
         area = request.GET['area']
         user_id = request.GET['user_id']
         
-    def svd(user_id, area) :
-        algo=SVDpp()
-        algo = SVDpp(n_factors=100, n_epochs=15)
-        # 3. train model 저장
-        file_name=os.path.expanduser('./dump')
-        #dump.dump(file_name, algo=algo) # 한번 학습하고 여기는 주석처리
-        _, algo=dump.load(file_name)
+        def svd(user_id, area) :
+            algo=SVDpp()
+            algo = SVDpp(n_factors=100, n_epochs=15)
+            # 3. train model 저장
+            file_name=os.path.expanduser('./dump')
+            #dump.dump(file_name, algo=algo) # 한번 학습하고 여기는 주석처리
+            _, algo=dump.load(file_name)
 
 
-        Area=pd.read_csv('./area.csv') ## { 상품아이디(학습데이터), area, 상품ID }
+            Area=pd.read_csv('./area.csv') ## { 상품아이디(학습데이터), area, 상품ID }
 
-        #nowarea="C"
-        #user=str("A2CX7LUOHB2NDG") # usre ID 받아오기
-        neww=Area[Area['Area']==area]['productID'].tolist() # 구역 받아오기
-        predictions=[algo.predict(str(user_id), str(productID)) for productID in neww] # 예측
-        ######
-        def sortkey_est(pred):
-          return pred.est
-        predictions.sort(key=sortkey_est, reverse=True)
-        #print(predictions)
-        top_product_id = [int(pred.iid) for pred in predictions]
-        top_product_id=top_product_id[:5]
-        return top_product_id
+            #nowarea="C"
+            #user=str("A2CX7LUOHB2NDG") # usre ID 받아오기
+            neww=Area[Area['Area']==area]['productID'].tolist() # 구역 받아오기
+            predictions=[algo.predict(str(user_id), str(productID)) for productID in neww] # 예측
+            ######
+            def sortkey_est(pred):
+            return pred.est
+            predictions.sort(key=sortkey_est, reverse=True)
+            #print(predictions)
+            top_product_id = [int(pred.iid) for pred in predictions]
+            top_product_id=top_product_id[:5]
+            return top_product_id
 
-#fortest
-tmp=svdclass.svd("A2CX7LUOHB2NDG", 'A')
-# return vlaue :
-#[594450209, 321732944, 594287995, 594033926, 594033934, 594296420, 594033896, 511189877, 594450705, 594451647, 594477670, 594017343, 594017580, 594202442, 594481813, 558835155, 594478162, 594012015, 439886341, 528881469]
+        #fortest
+        tmp=svdclass.svd("A2CX7LUOHB2NDG", 'A')
+        # return vlaue :
+        #[594450209, 321732944, 594287995, 594033926, 594033934, 594296420, 594033896, 511189877, 594450705, 594451647, 594477670, 594017343, 594017580, 594202442, 594481813, 558835155, 594478162, 594012015, 439886341, 528881469]
 
-print(tmp[0])
+        print(tmp[0])
 
         send = Send.objects.all().filter(product_id=top_product_id)
         serializer = SendSerializer(send, many=True)
