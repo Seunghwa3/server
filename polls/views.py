e41abf29de05dd27183cdc5fa4b790f9029d7428
Seunghwa3 <tmdghk3636@naver.com>
2020-12-09 13:58:28 +0900

    --

--- polls/views.py
+++ polls/views.py
@@ -24,7 +24,29 @@ class PostList(APIView):
         area = request.GET['area']
         user_id = request.GET['user_id']
         
+        def svd(user_id, area) :
+            algo = SVDpp()
+            algo = SVDpp(n_factors=100, n_epochs=15)
+            # 3. train model 저장
+            file_name=os.path.expanduser('./dump')
+            #dump.dump(file_name, algo=algo) # 한번 학습하고 여기는 주석처리
+            _, algo=dump.load(file_name)
 
+
+            Area=pd.read_csv('./area.csv') ## { 상품아이디(학습데이터), area, 상품ID }
+
+            #nowarea="C"
+            #user=str("A2CX7LUOHB2NDG") # usre ID 받아오기
+            neww=Area[Area['area']==area]['productID'].tolist() # 구역 받아오기
+            predictions=[algo.predict(str(user_id), str(productID)) for productID in neww] # 예측
+            ######
+            def sortkey_est(pred):
+                return pred.est
+            predictions.sort(key=sortkey_est, reverse=True)
+            #print(predictions)
+            top_product_id = [int(pred.iid) for pred in predictions]
+            top_product_id=top_product_id[:5]
+            return top_product_id
 
         send = Send.objects.filter(product_id="594450209")
         serializer = SendSerializer(send, many=True)
