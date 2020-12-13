b916bfd5eb57035b5be5be4e83215bd13e5f7e67
Seunghwa3 <tmdghk3636@naver.com>
2020-12-13 17:16:44 +0900

    --

--- server/settings.py
+++ server/settings.py
@@ -125,14 +125,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media")
 
 STATICFILES_DIRS = [
-    os.path.join(os.path.dirname(BASE_DIR), 'static', 'static_dirs'),
-    os.path.join(os.path.dirname(BASE_DIR), 'static', 'media'),
+    os.path.join(os.path.dirname(BASE_DIR), "static", "static_dirs"),
+    os.path.join(os.path.dirname(BASE_DIR), "static", "media"),
 ]
